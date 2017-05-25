89441b005df8a311f4f53aef2d7eae7c699c803e
shubham0677 <shubham.sharma0677@gmail.com>
2017-05-25 01:23:49 -0400

    Added uni tests for parameterized constructor and AddToRadius methods

---
+++ CircleRadius/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleRadius
+{
+    public class Circle
+    {
+        public double Radius { get; set; }
+
+        public Circle()
+        {
+            
+        }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public void AddToRadius(double input)
+        {
+            Radius += input;
+        }
+    }
+}
---
+++ CircleRadiusTests/CircleTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CircleRadius;
+
+namespace CircleRadiusTests
+{
+    [TestFixture]
+    class CircleTests
+    {
+        Circle circle;
+
+        [SetUp]
+        public void Init()
+        {
+            circle = new Circle();
+            circle.Radius = 5.0;
+        }
+
+        [Test]
+        public void CreateCircleWithRadius()
+        {
+            Circle testCircle = new Circle(5.0);
+            Assert.AreEqual(5.0, testCircle.Radius);
+        } 
+
+        [Test]
+        public void AddToRadius()
+        {
+            double input = 2.5;
+            circle.AddToRadius(input);
+            Assert.AreEqual(7.5,circle.Radius);
+        }
+
+        [Test]
+        public void AddNegativeValueToRadius()
+        {
+
+        }
+    }
+}
